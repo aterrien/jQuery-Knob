fd252a07d75ecfe800d71d01d83cde40c7f49fe8
Anthony Terrien <anthony.terrien@gmail.com>
2011-11-23 22:57:11 +0100

    callabacks

--- js/jquery.knob-1.0.0.js
+++ js/jquery.knob-1.0.0.js
@@ -16,110 +16,158 @@ $(function() {
     $.fn.knob = function( gopt ) {
 
         return this.each(
-                        function() {
+        
+                    function() {
 
-                            var $this = $(this);
+                        var $this = $(this);
 
-                            if( $this.data('knobed') ) return $this;
-                            $this.data('knobed',true);
+                        if( $this.data('knobed') ) return $this;
+                        $this.data('knobed',true);
 
-                            opt = $.extend(
-                                            {
-                                                'min' : $this.data('min') || 0
-                                                ,'max' : $this.data('max') || 100
-                                                ,'cursor' : $this.data('cursor')
-                                                ,'thickness' : $this.data('thickness') || .3
-                                                ,'width' : $this.data('width') || 200
-                                                ,'displayInput' : $this.data('displayinput')==null || $this.data('displayinput')
-                                                ,'fgColor' : $this.data('fgcolor') || '#87CEEB' //#222222'
-                                                ,'bgColor' : $this.data('bgcolor') || '#EEEEEE'
-                                                ,'readOnly' : $this.data('readonly')
-                                            }
-                                            ,gopt
-                                        );
-
-                            var c = $('<canvas width="'+opt.width+'" height="'+opt.width+'"></canvas>')
-                                //,ipt = $('<input type="text">')
-                                ,wd = $('<div style=width:'+opt.width+'px;display:inline;"></div>')
-                                ,k;
-
-                            $this.wrap( wd )
-                            $this.before( c );
-
-                            opt.displayInput
-                            && $this.css(
+                        var opt = $.extend(
                                         {
-                                        'width' : opt.width/2+'px'
-                                        ,'position' : 'absolute'
-                                        ,'margin-top' : (opt.width*5/13)+'px'
-                                        ,'margin-left' : '-'+3*opt.width/4+'px'
-                                        ,'font-size' : opt.width/5+'px'
-                                        ,'border' : 'none'
-                                        ,'background' : 'none'
-                                        ,'font-family' : 'Arial'
-                                        ,'font-weight' : 'bold'
-                                        ,'text-align' : 'center'
-                                        ,'color' : 'lightgrey'
-                                        ,'padding' : '0px'
-                                        ,'-webkit-appearance': 'none'
+                                            'min' : $this.data('min') || 0
+                                            ,'max' : $this.data('max') || 100
+                                            ,'cursor' : $this.data('cursor')
+                                            ,'thickness' : $this.data('thickness') || .3
+                                            ,'width' : $this.data('width') || 200
+                                            ,'displayInput' : $this.data('displayinput')==null || $this.data('displayinput')
+                                            ,'fgColor' : $this.data('fgcolor') || '#87CEEB' //#222222'
+                                            ,'bgColor' : $this.data('bgcolor') || '#EEEEEE'
+                                            ,'readOnly' : $this.data('readonly')
+                                            ,'draw' :
+                                                    /**
+                                                     * @param int a angle
+                                                     * @param int v current value
+                                                     * @param array opt plugin options
+                                                     * @param context ctx Canvas context 2d
+                                                     */
+                                                    function( a, v, opt, ctx ) {
+                                                            var sa = 1.5*Math.PI
+                                                                ,ea = sa+a
+                                                                ,r = opt.width/2
+                                                                ,lw = r*opt.thickness;
+
+                                                            ctx.clearRect(0, 0, opt.width, opt.width);
+                                                            ctx.lineWidth = lw;
+
+                                                            opt.cursor
+                                                                && ( sa = ea-0.3 )
+                                                                && ( ea = ea+0.3 );
+
+                                                            ctx.beginPath();
+                                                            ctx.strokeStyle = opt.fgColor;
+                                                            ctx.arc( r, r, r-lw, sa, ea, false);
+                                                            ctx.stroke();
+
+                                                            ctx.beginPath();
+                                                            ctx.strokeStyle = opt.bgColor;
+                                                            ctx.arc(
+                                                                        r, r, r-lw ,sa
+                                                                        ,(v==opt.min && !opt.cursor)
+                                                                            ? sa+0.0001
+                                                                            : ea
+                                                                        , true
+                                                                    );
+                                                            ctx.stroke();
+                                                        }
+                                            ,'change' :
+                                                    /**
+                                                     * @param int v Current value
+                                                     */
+                                                    function(v) {}
+                                            ,'release' :
+                                                    /**
+                                                     * @param int v Current value
+                                                     */
+                                                    function(v) {}
                                         }
-                                    )
-                            || $this.css(
-                                        {
-                                        'width' : '0px'
-                                        , 'visibility' : 'hidden'
-                                        }
+                                        ,gopt
                                     );
 
-                            k = new Knob( $this, c, opt );
-                            k.set( parseInt($this.val()) || 0 );
+                        var c = $('<canvas width="'+opt.width+'" height="'+opt.width+'"></canvas>')
+                            //,ipt = $('<input type="text">')
+                            ,wd = $('<div style=width:'+opt.width+'px;display:inline;"></div>')
+                            ,k;
 
-                            if( !opt.readOnly ){
-                                c.bind(
-                                    "mousedown touchstart"
-                                    ,function( e ) {
-                                        e.preventDefault();
-                                        k.startDrag( e );
+                        $this.wrap( wd )
+                        $this.before( c );
+
+                        opt.displayInput
+                        && $this.css(
+                                    {
+                                    'width' : opt.width/2+'px'
+                                    ,'position' : 'absolute'
+                                    ,'margin-top' : (opt.width*5/13)+'px'
+                                    ,'margin-left' : '-'+3*opt.width/4+'px'
+                                    ,'font-size' : opt.width/5+'px'
+                                    ,'border' : 'none'
+                                    ,'background' : 'none'
+                                    ,'font-family' : 'Arial'
+                                    ,'font-weight' : 'bold'
+                                    ,'text-align' : 'center'
+                                    ,'color' : 'lightgrey'
+                                    ,'padding' : '0px'
+                                    ,'-webkit-appearance': 'none'
+                                    }
+                                )
+                        || $this.css(
+                                    {
+                                    'width' : '0px'
+                                    , 'visibility' : 'hidden'
                                     }
                                 );
-                            }else{
-                                $this.attr('readonly','readonly');
-                            }
 
-                            return $this;
+                        k = new Knob( c, opt );
+                        k.onChange = function(v) {
+                                                    $this.val(v);
+                                                    opt.change(v);
+                                                 };
+                        k.onRelease = opt.release;
+                        k.set( parseInt($this.val()) || 0 );
+
+                        if( !opt.readOnly ){
+                            c.bind(
+                                "mousedown touchstart"
+                                ,function( e ) {
+                                    e.preventDefault();
+                                    k.startDrag( e );
+                                }
+                            );
+                        }else{
+                            $this.attr('readonly','readonly');
                         }
-                    );
+
+                        return $this;
+                    }
+                );
     }
 
-    Knob=function( ipt, c, opt ){
+    Knob = function( c, opt ) {
 
         var v = 0
             ,ctx = c[0].getContext("2d")
             ,a = Math.PI*0.0001
-            ,a2 = 2*Math.PI
-            ,r = opt.width/2
-            ,lw = r*opt.thickness
+            ,PI2 = 2*Math.PI
             ,mx ,my ,x ,y
-            ,self = this;
-            ;
-
-        this.set = function( _v ) {
-            v=( _v>opt.max ) ? opt.max : _v;
-            ipt.val(v);
-            a = (v-opt.min)*a2/(opt.max-opt.min);
-            this.draw();
+            ,_self = this;
+            
+        this.onChange = function() {}
+        this.onRelease = function() {}
+            
+        this.set = function(_v) {
+            a = (_v-opt.min)*PI2/(opt.max-opt.min);
+            opt.draw( a, _v, opt, ctx );
         }
 
         this.get = function() {
-            var dx = mx-x
-                ,dy = -(my-y-opt.width/2)
-                ,b = a = Math.atan2(dx,dy);
-
-            (a<0) && (b=a+a2);
-            return v = Math.round( b*(opt.max-opt.min)/a2 ) + opt.min;
+            var b = a = Math.atan2( mx-x, -(my-y-opt.width/2) );
+            (a<0) && (b=a+PI2);
+            v = Math.round( b*(opt.max-opt.min)/PI2 ) + opt.min;
+            return v = ( v>opt.max ) ? opt.max : v;
         }
 
-        this.capture = function( e ) {
+        this.capture = function(e) {
             switch( e.type ){
                 case 'mousemove' :
                 case 'mousedown' :
@@ -132,13 +180,15 @@ $(function() {
                     my = e.originalEvent.touches[0].pageY;
                     break;
             }
-            this.set( this.get() );
+
+            this.onChange( _v = this.get() );
+            this.set( _v );
         }
 
-        this.startDrag = function( e ) {
+        this.startDrag = function(e) {
 
             var p = c.position();
-
+            
             x = p.left+(opt.width/2);
             y = p.top;
 
@@ -146,49 +196,19 @@ $(function() {
             
             $(document).bind(
                             "mousemove.knob touchmove.knob"
-                            ,function( e ){
-                                self.capture(e);
+                            ,function(e) {
+                                _self.capture(e);
                             }
                         )
                         .bind(
-                            "mouseup touchend"
-                            ,function(){
-                                $(document).unbind('mousemove.knob touchmove.knob');
+                            "mouseup.knob touchend.knob"
+                            ,function() {
+                                $(document).unbind('mousemove.knob touchmove.knob mouseup.knob touchend.knob');
+                                _self.onRelease(v);
                             }
                         );
         }
-
-        this.draw = function() {
-
-            var _sa = 1.5*Math.PI
-                ,_ea = _sa+a;
-
-            ctx.clearRect(0, 0, opt.width, opt.width);
-            ctx.lineWidth = lw;
-
-            opt.cursor
-                && (_sa=_ea-0.3 )
-                && (_ea=_ea+0.3 );
-
-            ctx.beginPath();
-            ctx.strokeStyle = opt.fgColor;
-            ctx.arc( r, r, r-lw, _sa, _ea, false);
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.strokeStyle = opt.bgColor;
-            ctx.arc(
-                        r, r, r-lw
-                        ,_sa
-                        ,(v==opt.min && !opt.cursor)
-                            ? _sa+0.0001
-                            : _ea
-                        , true
-                    );
-            ctx.stroke();
-        };
     }
 
     $(".knob").knob();
 });
-
